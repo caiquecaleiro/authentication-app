adda2efc2d4970f1fb807300a42f4bf98aed0391
Caique Caleiro <caiquecaleiro@gmail.com>
2016-12-23 14:37:18 -0200

    Change server structure and add CORS

--- server/index.js
+++ server/index.js
@@ -3,14 +3,16 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
-const router = require('./router');
+const router = require('./src/router');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 // DB setup
 mongoose.connect('mongodb://localhost:auth/auth');
 
 // App setup
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
