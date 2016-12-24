2525d17613b41597702f8a761e5c4542ea677169
Caique Caleiro <caiquecaleiro@gmail.com>
2016-12-24 00:24:13 -0200

    Add Welcome component

---
+++ client/src/components/Welcome.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Welcome = () => {
+  return (
+    <h5>Welcome to the Redux Auth App!</h5>
+  );
+};
+
+export default Welcome;
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route } from 'react-router';
+import { Route, IndexRoute } from 'react-router';
 
 import App from './components/App';
 import SignIn from './components/auth/SignIn';
@@ -7,9 +7,11 @@ import SignOut from './components/auth/SignOut';
 import SignUp from './components/auth/SignUp';
 import RequireAuth from './components/auth/RequireAuth';
 import Feature from './components/Feature';
+import Welcome from './components/Welcome';
 
 export default (
   <Route path="/" component={App}>
+    <IndexRoute component={Welcome}/>
     <Route path="signin" component={SignIn} />
     <Route path="signout" component={SignOut} />
     <Route path="signup" component={SignUp} />
