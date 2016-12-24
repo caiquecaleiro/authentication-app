f3dba8b88d5ff5be02618a66a54a130b10b89a74
Caique Caleiro <caiquecaleiro@gmail.com>
2016-12-24 00:37:59 -0200

    Add to check whether there is a token available on localStorage to be used whenever the user refreshs the page

--- client/src/index.js
+++ client/src/index.js
@@ -7,11 +7,19 @@ import reduxThunk from 'redux-thunk';
 
 import reducers from './reducers';
 import routes from './routes';
+import { AUTH_USER } from './actions/types';
 
 const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
+const token = localStorage.getItem('token');
+
+if (token) {
+  // Updates the application state
+  store.dispatch({ type: AUTH_USER });
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory} routes={routes}/>
   </Provider>,
   document.getElementById('app')
